df285b5d4c9a968f03bbeb79df367f2f3642269e
Sen Wang <senwang@Sens-MacBook-Pro.local>
2017-12-05 18:05:33 -0600

    insert advertisement with javascript

---
+++ js/addiv.js
@@ -0,0 +1,23 @@
+let adDisplay = document.getElementById("adDiv")
+
+
+
+
+  //  let adContainer = document.createElement('div')
+  //  adContainer.className = adContainerStyle
+
+   let img = document.createElement("img")
+   img.src = "images/slider/slid2.jpg"
+
+
+
+
+  //  let ad1 = document.createElement("li")
+//    ad1.innerHTML =  "<img src='images/slider/slid1.jpg' width='16' height='16'>"
+  //  let ad2 = document.createElement("li")
+  //  ad1.innerHTML = "<img src='images/slider/slid2.jpg' width='16' height='16'>"
+  //  adContainer.appendChild(ad1)
+  //  adContainer.appendChild(ad2)
+  //  adDisplay.innerHTML = adContainer.value
+
+  adDisplay.appendChild(img)
